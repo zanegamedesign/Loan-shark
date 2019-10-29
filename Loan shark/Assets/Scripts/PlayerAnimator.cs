0a9da4247eadc39da2adbb3c27a2485ca26906c5
zane <djzanman@gmail.com>
2019-10-29 14:41:17 +1100

    Added character prefabs
    character pallets

---
+++ Loan shark/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+
+{
+    GameObject player;
+    public Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            print("here");
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Anim.SetBool("Is walking", true);
+        }
+        else
+        {
+            Anim.SetBool("Is walking", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Anim.SetBool("Is Jumping", true);
+        }
+        else
+        {
+            Anim.SetBool("Is Jumping", false);
+        }
+
+
+    }
+}
